bad781340e4acb7c6ac5b4c7dedb624ecb35baf2
AkyunaAkish <akyunaakish@gmail.com>
2015-06-03 11:42:59 -0600

    added findVowels function

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var foo = require("./var.js");
 console.log(foo.four);
 console.log(foo.zero);
 console.log(foo.prime(-2));
+console.log(foo.findVowels("I don't know if this sentence has any vowels."));
--- var.js
+++ var.js
@@ -20,4 +20,40 @@ module.exports = {
    }
    return result;
   },
+  findVowels:  function(str){
+  var arr = [];
+   if(str.indexOf('a') > -1){
+      arr.push("a");
+    }
+     if(str.indexOf('e') > -1){
+      arr.push("e");
+    }
+    if(str.indexOf('i') > -1){
+      arr.push("i");
+    }
+     if(str.indexOf('o') > -1){
+      arr.push("o");
+    }
+   if(str.indexOf('u') > -1){
+      arr.push("u");
+    }
+    if(str.indexOf('A') > -1){
+      arr.push("A");
+    }
+     if(str.indexOf('E') > -1){
+      arr.push("E");
+    }
+    if(str.indexOf('I') > -1){
+      arr.push("I");
+    }
+     if(str.indexOf('O') > -1){
+      arr.push("O");
+    }
+   if(str.indexOf('U') > -1){
+      arr.push("U");
+    }
+
+    arr += "," + arr.length + " vowels";
+    return arr.split(",");
+},
 };
