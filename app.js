ba4780113cb165dd8a166a6a49b41484ab385320
AkyunaAkish <akyunaakish@gmail.com>
2015-06-03 11:12:05 -0600

    added module functions

---
+++ app.js
@@ -0,0 +1,10 @@
+// 1. In a new repo, create two files: `lib.js` (or named something else) and `app.js`.
+// 2. lib.js should export an object with related functions.
+// 3. Import lib.js in app.js and call each of the related functions. Use `console.log`
+//  to confirm the functions are returning values when called.
+
+var foo = require("./var.js");
+
+console.log(foo.prime(-2));
+
+// var result = people.removeBadPeople([{name: 'A'}]);
---
+++ var.js
@@ -0,0 +1,26 @@
+module.exports = {
+  four: (function(){
+    return 2+2;
+  }()),
+  zero: (function(){
+    return 2-2;
+  }()),
+  prime: function(i){
+    var c = 2;
+    var result = true;
+    if(i < 0){
+      return false;
+    }
+    while(c < i){
+    if( i%c === 0){
+      result = false;
+      break;
+    }
+    c++;
+   }
+   return result;
+  },
+
+
+
+};
